317fcfe3cd34b4a7d878c74796a961c228408124
Sadeka Islam <islam.sadeka@gmail.com>
2020-06-01 23:09:02 +0600

    tensorflow prefix added in the keras import

--- models/keras_ssd300.py
+++ models/keras_ssd300.py
@@ -19,8 +19,8 @@ limitations under the License.
 from __future__ import division
 import numpy as np
 from tensorflow.keras.models import Model
-from keras.layers import Input, Lambda, Activation, Conv2D, MaxPooling2D, ZeroPadding2D, Reshape, Concatenate
-from keras.regularizers import l2
+from tensorflow.keras.layers import Input, Lambda, Activation, Conv2D, MaxPooling2D, ZeroPadding2D, Reshape, Concatenate
+from tensorflow.keras.regularizers import l2
 import tensorflow.keras.backend as K
 
 from keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
